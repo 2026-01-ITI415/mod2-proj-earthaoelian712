00b4fe650c5f7aa06cb0ff864ce0ac217d9325a8
earthaoelian712 <earthaoelian@gmail.com>
2026-02-26 19:15:40 -0500

    update1

--- Assets/__Scripts/Weapon.cs
+++ Assets/__Scripts/Weapon.cs
@@ -138,6 +138,12 @@ public class Weapon : MonoBehaviour
                 p = MakeProjectile();
                 p.transform.rotation = Quaternion.AngleAxis(-10, Vector3.back);
                 p.vel = p.transform.rotation * vel;
+                p = MakeProjectile();
+                p.transform.rotation = Quaternion.AngleAxis(+20, Vector3.back);
+                p.vel = p.transform.rotation * vel;
+                p = MakeProjectile();
+                p.transform.rotation = Quaternion.AngleAxis(-20, Vector3.back);
+                p.vel = p.transform.rotation * vel;
                 break;
 
         }
